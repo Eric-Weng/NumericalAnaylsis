9f9a6f7a5bd2dc96ac5e139d21aaf93d06532f45
Eric-Weng <weng.eric8@gmail.com>
2019-06-11 19:21:47 -0600

    Update FixedPoint.py

--- HW1/FixedPoint.py
+++ HW1/FixedPoint.py
@@ -3,14 +3,14 @@ import math
 
 def fixed_point_iteration(f, p, TOL):
     error = 1
-    iterations = 0
+    i = 0
     while error > TOL:
         p_new = f(p)
         error = abs(p_new - p)
         p = p_new
-        iterations += 1
-        print(f'p{iterations} = {p: 0.5f}')
-    print(f'Root: {p}\nIterations: {iterations}')
+        i += 1
+        print(f'p{i} = {p: 0.5f}')
+    print(f'Root: {p}\nIterations: {i}')
 
 if __name__ == '__main__':
     f = lambda x: (math.sin(x) + math.cos(x))/2
